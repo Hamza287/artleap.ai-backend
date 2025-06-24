97782641ab431fbe839c39a916cbecb54fdc3acc
MimarStudio <mimarstudiodev@gmail.com>
2025-06-24 11:04:33 +0500

    changes pushed

--- routers/watermarkRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/watermarkRoutes.js
-const express = require('express');
-const router = express.Router();
-const {  generateWithLogoWatermark } = require('./../controllers/watermarkController');
-
-router.post('/generate-with-watermark',  generateWithLogoWatermark);
-
-module.exports = router;
